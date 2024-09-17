18e5e5bf3b6084f778bda346eb3670f32e6a4026
uxitanq <sofiacom924@gmail.com>
2024-09-17 20:03:28 +0300

    Changes

--- l1/laba1.cpp
+++ l1/laba1.cpp
@@ -3,19 +3,25 @@
 #include <iomanip>
 
 int main (){
-  double k1, x;
-  std::cin >> k1 >> x;
+  double k1;
+  std::cout << "k = ";
+  std::cin >> k1;
+  double x;
+  std::cout << std::endl << "x =";
+  std::cin >> x;
   double k = 1;
   for (int i = 0; i < k1; i++)
       k *= 0.1;
   //std::cout << k << std::endl;
-  double res1 = sqrt (x + 1), res2 = 1;
+  double res1 = sqrt (x + 1), res2 = 0;
   double sl = 22222;
   int n = 1;
-  while (sl >= k){
-    if (n == 1)
+  while (fabs (sl) >= k){
+    if (n == 1){
       sl = 1;
-    sl *= abs ((2 *(n - 1) - 1));
+      res2 = 1;
+    }
+    sl *= abs ((2 * (n - 1) - 1));
     sl /= (2 * (n - 1) + 2);
     sl *= x;
     if (!(n % 2 == true))
